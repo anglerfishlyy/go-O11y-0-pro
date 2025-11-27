d0b9faf765ca34cf658f8ca599f258f6a07b778e
Kavya Samudrala <kavyalegitimate@gmail.com>
2025-11-27 19:21:15 +0530

    characteristics of go

---
+++ #2-why-go/characteristics.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func main() {
+    fmt.Println("simple!")
+    fmt.Println("fast-build")
+    fmt.Println("instant-start")
+    fmt.Println("lightweight")
+    fmt.Println("efficient")
+    fmt.Println("compiled")
+    fmt.Println("portable")
+}
---
+++ #2-why-go/go.mod
@@ -0,0 +1,3 @@
+module go-o11y
+
+go 1.23.3
