805c7d2bcaa968f8fe8b2704565bd40982983da6
anglerfishlyy <kavyasamudralaA7@outlook.com>
2025-11-27 22:38:51 +0530

    charateristics of go

--- #2-why-go/characteristics.go
+++ #2-why-go/characteristics.go
@@ -1,6 +1,7 @@
 // go mod init go-o11y   // creates a new module for your project
 // go mod tidy           // cleans and adds needed dependencies
 // go build              // builds the current folder into a binary
+// watch https://youtube.com/shorts/jiVeI8WHiOs?feature=share
 
 package main
 
